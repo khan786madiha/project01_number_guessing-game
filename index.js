31abf3e4e8e97eaf399f9ddb253f47da74e57901
Madiha Khan <mahaalikhanalizafarkhan@gmail.com>
2024-03-25 01:34:15 -0700

    Project01 CLI_number_guessing_game with typescript , node.js and inquirer..
    Madiha Khan's project01_CLI_number_guessing_game_with typescript , node.js and inquirer...

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const computer_randomNumber = Math.floor(Math.random() * 10 + 1);
+//  console.log(computer_randomNumber);
+const answer = await inquirer.prompt([
+    {
+        name: "your_GuessedNumber",
+        type: "number",
+        message: "please guess a number:"
+    }
+]);
+if (answer.your_GuessedNumber === computer_randomNumber) {
+    console.log("congratulations you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number, try again");
+}
